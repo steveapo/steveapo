0b083cac27306c9d91f8e26550a393c62e7d1069
steveapo <steveapo@icloud.com>
2020-07-29 05:36:47 +0300

    Created the Chapters system in order to keep track of each Chapter's IDs and organized everything into their own folder.

---
+++ Masters/master.js
@@ -0,0 +1,18 @@
+function MainMenu() {
+    location.href = "../Mains/main_menu.html";
+  }
+
+  function Achievements() {
+      location.href = "../Mains/achievements.html";
+  }
+
+  function Chapters(i) {
+      if (i == 0)
+      {
+          location.href = "../Chapters/chapter_1.html";
+      }
+      else if (i == 1)
+      {
+          location.href = "../Chapters/chapter_2.html"
+      }
+  }
