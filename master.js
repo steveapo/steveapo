daa3f7ba41f703230a7f60afd4c8084c6f6de38f
steveapo <steveapo@icloud.com>
2020-07-29 06:02:03 +0300

    It should work now that everything is not organized :)

---
+++ master.js
@@ -0,0 +1,18 @@
+function MainMenu() {
+    location.href = "main_menu.html";
+  }
+
+  function Achievements() {
+      location.href = "achievements.html";
+  }
+
+  function Chapters(i) {
+      if (i == 0)
+      {
+          location.href = "chapter_1.html";
+      }
+      else if (i == 1)
+      {
+          location.href = "chapter_2.html"
+      }
+  }
